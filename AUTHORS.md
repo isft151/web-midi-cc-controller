d07e168d7a5ef8aaec607857e37d458a8898c8fa
melifosque <melifosque@gmail.com>
2023-09-12 01:06:56 -0300

    diseño Meli

--- AUTHORS.md
+++
@@ -1,9 +0,0 @@
-# Authors, Students of ISFT 151:
-* ISFT151: 
-
-# Authors, Professors:
-* ISFT151: 
-    - Ferreira, Gabriel. 
-    - Machado, Sandra.
-    - Oemig, José Luis.
-    - Santiago, Matías.
--- OBXD-NODE-EDITOR/02-refact/app.js
+++ OBXD-NODE-EDITOR/02-refact/app.js
@@ -7,48 +7,46 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-// TODO HAY QUE PENSAR COMO ENVIAR ESTO UNA SOLA VEZ
 const midiOutput = new easymidi.Output('Client-128:Virtual RawMIDI 128:0');
 
+app.use(express.static(__dirname)); 
+
 // Ruta raíz
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/index.html');
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
 // Conexiones WebSocket
 wss.on('connection', (ws) => {
-    console.log('Cliente WebSocket conectado');
+  console.log('Cliente WebSocket conectado');
 
-    // Manejo de mensajes desde el navegador
-    ws.on('message', (message) => {
-        console.log(`Mensaje recibido del navegador: ${message}`);
+  // Manejo de mensajes desde el navegador
+  ws.on('message', (message) => {
+    console.log(`Mensaje recibido del navegador: ${message}`);
 
-        // Analizar el mensaje JSON
-        const data = JSON.parse(message);
-        const { ccNumber, ccValue} = data;
+    // Analizar el mensaje JSON
+    const data = JSON.parse(message);
+    const { ccNumber, ccValue } = data;
 
-        // Enviar el mensaje MIDI CC al dispositivo seleccionado
-        sendMidiCC(ccNumber, ccValue);
-    });
+    // Enviar el mensaje MIDI CC al dispositivo seleccionado
+    sendMidiCC(ccNumber, ccValue);
+  });
 });
 
 // Función para enviar un mensaje MIDI CC
 function sendMidiCC(ccNumber, ccValue) {
-    // Enviar el mensaje MIDI CC
-    midiOutput.send('cc', {
-        controller: ccNumber,
-        value: ccValue,
-        channel: 1, // Canal MIDI (cambia si es necesario)
-    });
+  // Enviar el mensaje MIDI CC
+  midiOutput.send('cc', {
+    controller: ccNumber,
+    value: ccValue,
+    channel: 1, // Canal MIDI (cambia si es necesario)
+  });
 
-    // Cerrar la conexión MIDI después de enviar el mensaje NO CERRAR!!!!
-    // midiOutput.close();
-
-    console.log(`Enviando CC ${ccNumber} a Client-129:Virtual RawMIDI 129:0: ${ccValue}`);
+  console.log(`Enviando CC ${ccNumber} a Client-128:Virtual RawMIDI 128:0: ${ccValue}`);
 }
 
-// Iniciar el servidor en el puerto 3001
+// Iniciar el servidor en el puerto 3000
 const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
-    console.log(`Servidor Node.js escuchando en el puerto ${PORT}`);
+  console.log(`Servidor Node.js escuchando en el puerto ${PORT}`);
 });
