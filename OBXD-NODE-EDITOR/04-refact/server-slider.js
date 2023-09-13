aa7a8d51deb163f7cf0cd980ca6fce4cbd614fb5
NicolasVallejos <nicolasvallejos219@gmail.com>
2023-09-13 08:04:52 -0300

    Actualizo codigo server e index para arreglar la animacion.

--- OBXD-NODE-EDITOR/04-refact/server-slider.js
+++ OBXD-NODE-EDITOR/04-refact/server-slider.js
@@ -4,9 +4,18 @@ const path = require('path');
 
 const PORT = process.env.PORT || 8080;
 
-// Servir solo los archivos JavaScript que deseas
+
 app.use('/three.module.js', express.static(path.join(__dirname, '/js/three.module.js')));
 app.use('/threejs-toys.module.min.js', express.static(path.join(__dirname, '/js/threejs-toys.module.min.js')));
+app.use('/style-slider-nv.css', express.static(path.join(__dirname,'/css/style-slider-nv.css')))
+
+
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index-slider-nv.html'));
+});
+
 
 app.listen(PORT, () => {
   console.log(`Servidor en ejecución en http://localhost:${PORT}`);
